b8c432507de3e476741e719e1c2f0e071b93d674
Yury Ostroukh <0str0uchura@gmail.com>
2017-12-25 18:28:38 +0300

    Add router for actions with nodes

--- src/server/app.js
+++ src/server/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const routes = require('./routes');
+const nodesRouter = require('./routes/nodes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 // Routes
 app.use('/', routes);
+app.use('/nodes', nodesRouter);
 
 module.exports = app;
---
+++ src/server/routes/nodes.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const blockchain = require('../setup//blockchain');
+const router = express.Router();
+
+router.put('/register', (req, res) => {
+  const {nodes} = req.body;
+
+  if (!nodes) {
+    res
+      .status(400)
+      .send({message: 'Missing field "nodes".'});
+    return;
+  }
+
+  nodes.forEach(blockchain.registerNode.bind(blockchain));
+  res
+    .status(201)
+    .send({
+      message: 'Nodes were added',
+      totalNodes: [...blockchain.nodes],
+    });
+});
+
+router.get('/resolve', async (req, res) => {
+  const isReplaced = await blockchain.resolveConflicts();
+  let resObject;
+
+  if (isReplaced) {
+    resObject = {
+      message: 'Chain was replaced.',
+      chain: blockchain.chain,
+    };
+  } else {
+    resObject = {
+      message: 'Chain is authoritative.',
+      chain: blockchain.chain,
+    };
+  }
+
+  res
+    .status(200)
+    .send(resObject);
+});
+
+module.exports = router;
